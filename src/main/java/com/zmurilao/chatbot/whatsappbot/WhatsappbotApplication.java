8a9b9acae352d50057f042cf74f758a91e80a351
Murilo Braz <159815893+zmurilaoo@users.noreply.github.com>
2025-02-13 17:38:12 -0300

    ChatBot Spring

---
+++ src/main/java/com/zmurilao/chatbot/whatsappbot/WhatsappbotApplication.java
@@ -0,0 +1,21 @@
+package com.zmurilao.chatbot.whatsappbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class WhatsappbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappbotApplication.class, args);
+
+		JOptionPane.showMessageDialog(null,"Testando..");
+
+
+
+	}
+
+
+}
---
+++ src/test/java/com/zmurilao/chatbot/whatsappbot/WhatsappbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zmurilao.chatbot.whatsappbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
